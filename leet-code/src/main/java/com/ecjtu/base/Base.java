646e63723d2f0b907e36e5cea554c816b826fb84
xiexiang <xiexiang@xiexiang.local>
2019-07-16 15:58:50 +0800

    add readme

---
+++ leet-code/src/main/java/com/ecjtu/base/Base.java
@@ -0,0 +1,13 @@
+package com.ecjtu.base;
+
+/**
+ * @author: xiexiang
+ * @create: 2019/07/16 15:58
+ */
+public class Base {
+
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
