afdd438a201d7c1c6c4756472cb611d9fb1ebdb0
xiexiang <xiexiang@hzyushi.cn>
2020-01-31 09:59:17 +0800

    commit code

--- tool/src/main/resources/ddl/ddl.sql
+++ tool/src/main/resources/ddl/ddl.sql
@@ -2,4 +2,10 @@ ALTER TABLE `eusdk`.`uadmin`
 MODIFY COLUMN `adminGames` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL COMMENT '管理游戏' AFTER `adminRoleName`;
 
 ALTER TABLE `eusdk`.`uadmin`
-MODIFY COLUMN `adminGames` varchar(2048) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '管理游戏' AFTER `adminRoleName`;
+MODIFY COLUMN
+
+`adminGames` varchar(2048) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '管理游戏' AFTER `adminRoleName`;
+
+
+
+UPDATE `pay_provider_param_tmpl` set `param_key` ='signCert' where id=14 and `param_key` ='acpProdSign'
